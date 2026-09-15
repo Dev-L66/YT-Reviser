58c8f300830e4743971ac65e3430f790c43f09e2
Dev-L66 <sabeebashil@yahoo.com>
2026-09-15 21:24:55 +0300

    chunks

---
+++ backend/chunks.py
@@ -0,0 +1,49 @@
+import json
+
+with open("transcripts/transcript.json","r", encoding="utf-8") as f:
+    transcript = json.load(f)
+
+
+
+chunks = []
+
+current_text = []
+chunk_start = None
+chunk_end = None
+
+
+for segment in transcript:
+    if chunk_start is None:
+        chunk_start = segment["start"]
+
+
+    current_text.append(segment["text"])
+    chunk_end = segment["end"]
+
+    if chunk_start + chunk_end >=30:
+        chunks.append({
+            "start": chunk_start,
+            "end": chunk_end,
+            "text": "".join(current_text)
+        })
+
+
+    current_text = []
+    chunk_start = None
+    chunk_end = None
+
+
+if current_text:
+    chunks.append({
+        "start": chunk_start,
+        "end": chunk_end,
+        "text":"".join(current_text)
+    })
+
+
+with open("chunks.json", "w", encoding = "utf-8") as f:
+    json.dump(chunks, f, ensure_ascii=False, indent=2 )
+
+
+print(f"Created {len(chunks)} chunks")
+
